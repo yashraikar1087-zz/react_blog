f4b8ef0315f61061613501db9adc4487416375c7
yashraikar1087 <yashraikar1087@gmail.com>
2019-03-12 21:08:08 -0600

    create new action creator to handle multiple fetch user requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  console.log("about to fetch posts");
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => {
   return async function(dispatch) {
     const response = await jsonPlaceholder.get("/posts");
@@ -13,11 +20,17 @@ export const fetchPosts = () => {
 //   dispatch({ type: "FETCH_USER", payload: response.data });
 // };
 
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
+//memoized version of this approach
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
+
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
